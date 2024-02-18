9968ded13093b59f0364bbda222d2c5947ca5f1b
Camden Webster <cwebster@students.mchenry.edu>
2024-02-18 11:07:13 -0600

    Clean up boilerplate

--- FamilyTreeCamdenW/FamilyTreeCamdenW/Family.swift
+++ FamilyTreeCamdenW/FamilyTreeCamdenW/Family.swift
@@ -1,65 +1,12 @@
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
+//
+//  Family.swift
+//  FamilyTreeCamdenW
+//
+//  Created by Camden Webster on 2/14/24.
+//
 import UIKit
 
-// MARK:- Reusable SFSymbol Images
-//enum FamilySymbols {
-//  case bookmark
-//  case bookmarkFill
-//  case book
-//  case letterSquare(letter: Character?)
-//  
-//  var image: UIImage {
-//    let imageName: String
-//    switch self {
-//    case .bookmark, .book:
-//      imageName = "\(self)"
-//    case .bookmarkFill:
-//      imageName = "bookmark.fill"
-//    case .letterSquare(let letter):
-//      guard let letter = letter?.lowercased(),
-//      let image = UIImage(systemName: "\(letter).square")
-//        else {
-//          imageName = "square"
-//          break
-//      }
-//      return image
-//    }
-//    return UIImage(systemName: imageName)!
-//  }
-//}
-
-// MARK:- Library
+// MARK:- Family
 enum Family {
     static let familyMembers: [Person] = [
         Person(id: 1, firstName: "John", lastName: "Smith", spouseID: 2, birthDate: Date(timeIntervalSince1970: -2835940370), parentIDs: [0], maidenName: "none"),
@@ -119,25 +66,3 @@ func findSpouseName(_ familyMember: Person) -> String {
     }
     return spouseString
 }
-    
-//  
-//  static func saveImage(_ image: UIImage, forPerson person: Person) {
-//      let imageURL = FileManager.documentDirectoryURL.appendingPathComponent("-\(person.id)")
-//      if let jpgData = image.jpegData(compressionQuality: 0.7) {
-//        try? jpgData.write(to: imageURL, options: .atomicWrite)
-//      }
-//    }
-//    
-//    static func loadImage(forPerson person: Person) -> UIImage? {
-//      let imageURL = FileManager.documentDirectoryURL.appendingPathComponent("-\(person.id)")
-//      return UIImage(contentsOfFile: imageURL.path)
-//    }
-//  }
-//
-//
-//  extension FileManager {
-//    static var documentDirectoryURL: URL {
-//      return `default`.urls(for: .documentDirectory, in: .userDomainMask)[0]
-//    }
-//  }
-
